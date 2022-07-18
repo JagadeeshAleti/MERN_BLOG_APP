b096ac13a61fb357b024f3931d4d8feaf3b6845f
jagadeeshaleti <jagadeswara@ecyber.com>
2022-07-18 17:16:00 +0530

    No posts founf is fixed

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -8,11 +8,14 @@ import { useLocation } from "react-router";
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
+  const [empty, setEmpty] = useState(false);
   const { search } = useLocation();
 
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await axios.get("/posts" + search);
+      console.log(res.data.length);
+      res.data.length === 0 ? setEmpty(true) : setEmpty(false);
       setPosts(res.data);
     };
     fetchPosts();
@@ -21,7 +24,21 @@ export default function Home() {
     <>
       <Header />
       <div className="home">
-        <Posts posts={posts} />
+        {empty ? (
+          <div className="no-posts">
+            <h1 className="message">
+              No post yet, Please browse some other category for more posts!! Or
+              write your post !!
+            </h1>
+            <img
+              src="https://cdn.dribbble.com/users/2460221/screenshots/14347554/media/6ad1d1eba400880e3c3de062fcb13576.png?compress=1&resize=400x300&vertical=top"
+              alt=""
+            />
+          </div>
+        ) : (
+          <Posts posts={posts} />
+        )}
+
         <Sidebar />
       </div>
     </>
